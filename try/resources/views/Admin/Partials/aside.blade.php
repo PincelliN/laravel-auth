434d5bf671f51b34fc3c1de4fab168930dc7439d
Nicolò Pincelli <pincelli.n@gmail.com>
2024-09-23 16:41:06 +0200

    change layout  Dashboard home

---
+++ try/resources/views/Admin/Partials/aside.blade.php
@@ -0,0 +1,8 @@
+<aside class="pt-3">
+    <ul>
+        <li><a href=""><i class="fa-solid fa-house px-1"></i> Dashboard Home</a></li>
+        <li><a href=""><i class="fa-solid fa-table px-1"></i>Portfolio Index</a></li>
+        <li><a href=""><i class="fa-solid fa-plus px-1"></i>Aggiungi Progetto</a></li>
+    </ul>
+
+</aside>
