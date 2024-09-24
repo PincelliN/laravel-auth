186e4ba1698d12dbbc88a22d24542da64268616d
Nicolò Pincelli <pincelli.n@gmail.com>
2024-09-24 08:32:39 +0200

    Fix table and card

--- try/resources/views/layouts/app.blade.php
+++ try/resources/views/layouts/app.blade.php
@@ -28,7 +28,10 @@
     <div id="app" class="text-bg-dark">
         @include('Admin.Partials.header')
         <main class="d-flex py-2 px-3">
-            @include('Admin.Partials.aside')
+            @auth
+                @include('Admin.Partials.aside')
+            @endauth
+
             @yield('content')
         </main>
         @include('Admin.Partials.footer')
